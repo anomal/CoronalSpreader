957fd9e15df467848c1f838675f3cd9fa2ac954d
Sophiah Ho <sophiah.ho@gmail.com>
2020-04-23 18:09:56 -0400

    reorganize structure

---
+++ src/covid19sim.py
@@ -0,0 +1,436 @@
+import math
+from random import random
+from enum import Enum
+from collections import deque
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+populationSize = 2048
+ratioNursesInPopulation = 0.015
+infectiousness = 0.15
+ppeProtection = 0.95
+proportionSevere = 0.2
+proportionSevereCritical = 0.25
+recoveryTime = 18
+fatalityRate = 0.01
+maxPatientsPerNurse = 4
+totalDays = 240
+icuBedsPerHundredThousand = 13.5
+strikeDays = 0
+ppeArrivalDay = 9999999
+prioritizeNursePatient = False
+
+# Model
+
+class Outcome(Enum):
+    UNINFECTED = "Uninfected"
+    INFECTED = "Infected"
+    DEAD = "Dead"
+    RECOVERED = "Recovered"
+
+class Severity(Enum):
+    MILD = "Mild"
+    NONINV_VENT = "Hosp, non-inv vent"
+    INV_VENT = "Hosp, inv vent"
+
+class Position:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return str(self.x) + "," + str(self.y)
+
+class Person:
+
+    label = "Regular"
+
+    def __init__(self, position):
+        self.position = position
+        self.infectionDay = None
+        self.outcome = Outcome.UNINFECTED
+        self.severity = None
+        self.nurse = None
+
+    def __str__(self):
+        return str(self.position)
+
+    def isDead(self):
+        return self.outcome is Outcome.DEAD
+
+    def isSevere(self):
+        return self.severity is Severity.NONINV_VENT or self.severity is Severity.INV_VENT
+
+    def isRecovered(self):
+        return self.outcome is Outcome.RECOVERED
+
+    def infect(self, severity):
+        if self.outcome is Outcome.UNINFECTED:
+            self.outcome = Outcome.INFECTED
+            self.severity = severity
+            self.infectionDay = 1
+
+    def progress(self):
+        self.infectionDay += 1
+        if self.isSevere() and self.infectionDay == recoveryTime + 1 and random() * proportionSevere < fatalityRate:
+            self.die()
+        elif not self.isRecovered() and not self.isDead() and self.infectionDay is not None and self.infectionDay == recoveryTime + 1:
+            self.releaseNurse()
+            hospital.releaseIcuBed(self)
+            self.outcome = Outcome.RECOVERED
+
+    def die(self):
+        self.releaseNurse()
+        hospital.releaseIcuBed(self)
+        self.outcome = Outcome.DEAD
+
+    def releaseNurse(self):
+        if self.nurse is not None:
+            self.nurse.patients.remove(self)
+            self.nurse = None
+
+class Nurse(Person):
+
+    label = "Nurse"
+
+    def __init__(self, position):
+        super().__init__(position)
+        self.patients = []
+
+    def patientsStr(self):
+        s = ""
+        for patient in self.patients:
+            if s != "":
+                s += "; "
+            s += str(patient)
+        return "{ " + s + " }"
+
+class Hospital:
+
+    def __init__(self, totalIcuBeds):
+        self.reset(totalIcuBeds)
+
+    def reset(self, totalIcuBeds):
+        self.nurses = deque()
+        self.totalIcuBeds = totalIcuBeds
+        self.occupiedBeds = set()
+        self.nurseColleagues = {}
+
+    def assignNurse(self, person):
+        maxedOut = 0
+        totalNurses = len(self.nurses)
+        
+        while maxedOut != totalNurses:
+            nurse = self.nurses.popleft()
+            self.nurses.append(nurse)
+            if not nurse.isDead() and (not nurse.isSevere() or nurse.isRecovered()) and len(nurse.patients) < maxPatientsPerNurse:
+                nurse.patients.append(person)
+                person.nurse = nurse
+                return True
+            else:
+                maxedOut += 1
+
+        if prioritizeNursePatient and isinstance(person, Nurse):
+            newestNonNursePatient = self.findNewestNonNursePatient()
+            if newestNonNursePatient is None:
+                return False
+            else:
+                nurseToFree = newestNonNursePatient.nurse
+                newestNonNursePatient.die()
+                nurseToFree.patients.append(person)
+                person.nurse = nurseToFree
+                return True
+        else:
+            return False
+    
+    def findNewestNonNursePatient(self):
+        newest = None
+        for nurse in self.nurses:
+            for patient in nurse.patients:
+                if not isinstance(patient, Nurse) and (newest is None or patient.infectionDay < newest.infectionDay):
+                    newest = patient
+        return newest
+
+    def findNonNurse(self, patients):
+        for patient in patients:
+            if not isinstance(patient, Nurse):
+                return patient
+        return None
+
+    def assignIcuBed(self, patient):
+        bedAvailable = False
+        if len(self.occupiedBeds) < self.totalIcuBeds:
+            bedAvailable = True
+        elif prioritizeNursePatient and isinstance(patient, Nurse):
+            nonNurse = self.findNonNurse(self.occupiedBeds)
+            if nonNurse is not None:
+                nonNurse.die()
+                bedAvailable = True
+        if bedAvailable:
+            self.occupiedBeds.add(patient)
+            return True
+        else:
+            return False
+
+    def releaseIcuBed(self, patient):
+        self.occupiedBeds.discard(patient)
+
+# Functions
+
+def validCoordinate(x, y):
+    return x >= 0 and x < width and y >= 0 and y < height
+
+def getNeighbours(person):
+    neighbours = []
+    for n in [[-1,0], [1,0], [0,-1], [0,1]]:
+        x = person.position.x + n[0]
+        y = person.position.y + n[1]
+        if validCoordinate(x, y):
+            index = y * width + x
+            neighbour = people[index]
+            if neighbour.infectionDay == None:
+                neighbours.append(neighbour)
+    return neighbours
+
+def getColleagues(nurse):
+    colleagues = hospital.nurseColleagues[nurse]
+    if len(colleagues) < 1:
+        raise ValueError("Unexpected number of colleagues")
+    else:
+        return colleagues
+
+def findNurse(patient, strike):
+    if strike or not hospital.assignNurse(patient):
+        patient.die()
+
+def getExposureResult(hasPpe):
+    if hasPpe:
+        protection = ppeProtection
+    else:
+        protection = 0
+
+    if random() < infectiousness * (1 - protection):
+        if random() < proportionSevere:
+            if random() < proportionSevereCritical:
+                return Severity.INV_VENT
+            else:
+                return Severity.NONINV_VENT
+        else:
+            return Severity.MILD
+    else:
+        return None
+
+def expose(person, newlyInfected, strike, hasPpe):
+    if person.infectionDay == None and not person.isDead() and not person.isRecovered():
+        if isinstance(person, Nurse):
+            exposureResult = getExposureResult(hasPpe)
+        else:
+            exposureResult = getExposureResult(False)
+
+        if exposureResult is not None:
+            person.infect(exposureResult)
+            newlyInfected.append(person)
+
+            if exposureResult is not Severity.MILD:
+
+                if exposureResult is Severity.INV_VENT and not hospital.assignIcuBed(person):
+                    person.die()
+
+                if isinstance(person, Nurse):
+                    for patient in person.patients.copy():
+                        patient.releaseNurse()
+                        findNurse(patient, strike)
+                    person.patients = []
+
+                if not person.isDead():
+                    findNurse(person, strike)
+
+def spread(strike, hasPpe):
+    newlyInfected = []
+    for person in infected:
+        if not person.isDead() and not person.isSevere() and not person.isRecovered():
+            neighbours = getNeighbours(person)
+            for neighbour in neighbours:
+                expose(neighbour, newlyInfected, strike, hasPpe)
+        if person.nurse != None:
+            expose(person.nurse, newlyInfected, strike, hasPpe)
+        elif isinstance(person, Nurse):
+            colleagues = getColleagues(person)
+            for colleague in colleagues:
+                expose(colleague, newlyInfected, strike, hasPpe)
+        person.progress()
+    infected.extend(newlyInfected)
+        
+def aggregations(df):
+    return df.groupby(["day"]) \
+        .sum()[{"wasInfected", "isDead", "isRecovered", "newlyInfected","newlyInfectedSevere","wasInfectedNurse", "isDeadNurse"}] \
+        .reset_index() \
+        .rename(columns={
+            "day" : "Day",
+            "wasInfected" : "Total Infections",
+            "isDead" : "Total Dead",
+            "isRecovered" : "Total Recovered",
+            "newlyInfected" : "New Infections",
+            "newlyInfectedSevere" : "New Infections Requiring Hospitalization",
+            "wasInfectedNurse" : "Total Nurse Infections",
+            "isDeadNurse" : "Total Nurses Dead"
+    })
+
+def run():
+    data = {}
+    initPopulation()
+    collectData(data, 1)
+    for i in range(1, totalDays):
+        spread(i <= strikeDays, i >= ppeArrivalDay)
+        collectData(data, i + 1)
+    return pd.DataFrame(data=data)
+
+def collectData(data, day):
+    if data.get("day", None) is None:
+        data["day"] = []
+        data["id"] = []
+        data["legend"] = []
+        data["x"] = []
+        data["y"] = []
+        data["isNurse"] = []
+        data["severity"] = []
+        data["isDead"] = []
+        data["isRecovered"] = []
+        data["wasInfected"] = []
+        data["newlyInfected"] = []
+        data["newlyInfectedSevere"] = []
+        data["wasInfectedNurse"] = []
+        data["isDeadNurse"] = []
+    for person in people:
+        data["day"].append(day)
+        data["id"].append(str(person))
+        data["legend"].append(legend(person)) 
+        data["x"].append(person.position.x)  
+        data["y"].append(person.position.y)  
+        data["isNurse"].append(isinstance(person,Nurse))
+        if person.severity is None:
+            severityValue = "N/A"
+        else:
+            severityValue = person.severity.value
+        data["severity"].append(severityValue)
+        data["isDead"].append(person.isDead())
+        data["isRecovered"].append(person.isRecovered())
+        data["wasInfected"].append(person.outcome is not Outcome.UNINFECTED)
+        data["newlyInfected"].append(person.infectionDay == 1)
+        data["newlyInfectedSevere"].append(person.infectionDay == 1 and person.isSevere())
+        data["wasInfectedNurse"].append(isinstance(person,Nurse) and person.outcome is not Outcome.UNINFECTED)
+        data["isDeadNurse"].append(isinstance(person,Nurse) and person.isDead())
+
+def getTotalIcuBeds():
+    return max(1, round(populationSize * icuBedsPerHundredThousand/100000))
+
+width = 64
+height = math.floor(populationSize / width)
+hospital = Hospital(getTotalIcuBeds())
+people = []
+infected = []
+
+def isToBeNurse(i):
+    if ratioNursesInPopulation == 0:
+        return False
+    else:
+        divisor = 3
+        peoplePerNurse = 1/ratioNursesInPopulation
+        for n in range(divisor):
+            if i % divisor == n and i % round(peoplePerNurse) == round(peoplePerNurse * n/divisor):
+                return True 
+        return False
+    #return ratioNursesInPopulation != 0 and ( \
+     #   i % 3 == 0 and i % round(1/ratioNursesInPopulation) == 0 \
+      #  or i % 3 == 1 and i % round(1/ratioNursesInPopulation) == round(1/ratioNursesInPopulation*1/3) \
+       # or i % 3 == 2 and i % round(1/ratioNursesInPopulation) == round(1/ratioNursesInPopulation*2/3) \
+    #)
+
+def initPopulation():
+    people.clear()
+    infected.clear()
+    hospital.reset(getTotalIcuBeds())
+    prevNurse = None
+
+    for i in range(0, populationSize):
+        position = Position(i % width, math.floor(i / width))
+
+        if isToBeNurse(i):
+            person = Nurse(position)
+            hospital.nurseColleagues[person] = []
+
+            if prevNurse is not None:
+                # add previous nurse as colleague of current nurse
+                hospital.nurseColleagues[person].append(prevNurse)
+                # add current nurse as colleague of previous nurse
+                hospital.nurseColleagues[prevNurse].append(person)
+
+            prevNurse = person
+
+            if i % 2 == 0:
+                hospital.nurses.append(person)
+            else:
+                hospital.nurses.appendleft(person)
+        else:
+            person = Person(position)
+        if i == math.floor(populationSize / 2) + math.floor(width / 2) :
+            person.infect(Severity.MILD)
+            infected.append(person)
+        people.append(person)
+
+def legend(person):
+
+    legend = person.label
+
+    if person.outcome is Outcome.UNINFECTED:
+        legend += " " + Outcome.UNINFECTED.value
+    elif person.isDead():
+        legend += " " + Outcome.DEAD.value
+    elif person.isRecovered():
+        legend += " " + Outcome.RECOVERED.value
+    else: 
+        if person.severity is not None:
+            legend += " " + person.severity.value
+        else:
+            raise ValueError(person, "is infected but with no severity")
+
+    return legend
+
+def styleMarker(datum):
+    if (Nurse.label in datum.name): 
+        datum.marker.symbol = "cross"
+        if Severity.MILD.value in datum.name:
+            datum.marker.color = "orange"
+        elif Severity.INV_VENT.value in datum.name:
+            datum.marker.color = "purple"
+        elif Severity.NONINV_VENT.value in datum.name:
+            datum.marker.color = "red"
+        elif Outcome.DEAD.value in datum.name:
+            datum.marker.color = "white"
+        elif Outcome.RECOVERED.value in datum.name:
+            datum.marker.color = "green"
+        else:
+            datum.marker.color = "blue"
+    else:
+        if Severity.MILD.value in datum.name:
+            datum.marker.color = "navajowhite"
+        elif Severity.INV_VENT.value in datum.name:
+            datum.marker.color = "mediumpurple"
+        elif Severity.NONINV_VENT.value in datum.name:
+            datum.marker.color = "darksalmon"
+        elif Outcome.DEAD.value in datum.name:
+            datum.marker.color = "black"
+        elif Outcome.RECOVERED.value in datum.name:
+            datum.marker.color = "lightgreen"
+        else:
+            datum.marker.color = "lightskyblue"
+
+def showSpread(df, day):
+    dfDay = df[df["day"] == day]
+    fig = px.scatter(dfDay, x="x", y="y", color="legend")
+    for d in fig['data']:
+        styleMarker(d)
+    fig.show()
+
---
+++ test/test_covid19sim.py
@@ -0,0 +1,183 @@
+import sys
+sys.path.append('../src/')
+import unittest
+import math
+import numpy as np
+import covid19sim as sim
+
+class TestCovid19Sim(unittest.TestCase):
+
+    def setUp(self):
+        sim.populationSize = 2048
+        sim.ratioNursesInPopulation = 0.015
+        sim.infectiousness = 0.15
+        sim.ppeProtection = 0.95
+        sim.proportionSevere = 0.2
+        sim.proportionSevereCritical = 0.25
+        sim.recoveryTime = 18
+        sim.fatalityRate = 0.01
+        sim.maxPatientsPerNurse = 4
+        sim.totalDays = 240
+        sim.icuBedsPerHundredThousand = 13.5
+        sim.strikeDays = 0
+        sim.ppeArrivalDay = 9999999
+        sim.prioritizeNursePatient = False
+        sim.initPopulation()
+
+    def test_00whenGetExposureResult_expectInfection15(self):
+        total = 200000
+        count = 0
+        severeCount = 0
+        criticalCount = 0
+        ep = 0.05
+        for i in range(total):
+            severity = sim.getExposureResult(False)
+            if severity is not None:
+                count += 1
+            if severity is sim.Severity.INV_VENT:
+                criticalCount += 1
+                severeCount += 1
+            elif severity is sim.Severity.NONINV_VENT:
+                severeCount += 1
+        avg = count / total
+        isInExpectedRange = avg * (1-ep) < sim.infectiousness and avg * (1+ep) > sim.infectiousness
+        if not isInExpectedRange:
+            expectedCount = sim.infectiousness * total
+            print("expected avg:", sim.infectiousness, "real avg:", avg, "expected count:", expectedCount, "real count", count)
+        self.assertTrue(isInExpectedRange)
+        severePerTotal = severeCount / total
+        severityIsExpected = severePerTotal * (1-ep) < sim.proportionSevere * sim.infectiousness and severePerTotal * (1+ep) > sim.proportionSevere * sim.infectiousness
+        if not severityIsExpected:
+            print("expected severity proportion:", sim.proportionSevere * sim.infectiousness, "actual severity proportion:", severePerTotal, \
+                "expected severe cases:", total * sim.infectiousness * sim.proportionSevere, "actual severe cases", severeCount)
+        self.assertTrue(severityIsExpected)
+        criticalPerTotal = criticalCount / total
+        criticalIsExpected = criticalPerTotal * (1-ep) < sim.proportionSevereCritical * sim.proportionSevere * sim.infectiousness \
+            and criticalPerTotal * (1+ep) > sim.proportionSevereCritical * sim.proportionSevere * sim.infectiousness
+        if not criticalIsExpected:
+            print("expected critical:", sim.proportionSevereCritical * sim.proportionSevere * sim.infectiousness, ", actual critical:", criticalPerTotal, \
+                ", expected crit count:", total * sim.proportionSevereCritical * sim.proportionSevere * sim.infectiousness, ", actual crit count:", criticalCount)
+        self.assertTrue(criticalIsExpected)
+
+    def test_00whenAssignNurseAllFullOnly_expectFalse(self):
+        expectedCapacity = len(sim.hospital.nurses) * sim.maxPatientsPerNurse
+        patients = []
+        for i in range(expectedCapacity):
+            patient = sim.Person(sim.Position(0, i))
+            patients.append(patient)
+            self.assertTrue(sim.hospital.assignNurse(patient))
+        p = sim.Person(sim.Position(0, expectedCapacity))
+        self.assertFalse(sim.hospital.assignNurse(p))
+        for nurse in sim.hospital.nurses:
+           self.assertEqual(sim.maxPatientsPerNurse, len(nurse.patients))
+        patients[0].releaseNurse()
+        self.assertTrue(sim.hospital.assignNurse(p))
+
+    def test_00whenAssignNurseAllFullAndPrioritizeNurse_expectNurseGetsCare(self):
+        sim.prioritizeNursePatient = True
+        sim.initPopulation()
+        expectedCapacity = len(sim.hospital.nurses) * sim.maxPatientsPerNurse
+        patients = []
+        for i in range(expectedCapacity):
+            patient = sim.Person(sim.Position(0, i))
+            patients.append(patient)
+            patient.infect(sim.Severity.NONINV_VENT)
+            self.assertTrue(sim.hospital.assignNurse(patient))
+        p = sim.Person(sim.Position(0, expectedCapacity))
+        self.assertFalse(sim.hospital.assignNurse(p))
+        nurse = sim.Nurse(sim.Position(0, expectedCapacity))
+        self.assertTrue(sim.hospital.assignNurse(nurse))
+        for nurse in sim.hospital.nurses:
+           self.assertEqual(sim.maxPatientsPerNurse, len(nurse.patients))
+        sim.prioritizeNursePatient = False
+        sim.initPopulation()
+
+    def test_00whenAssignIcuBedToNurseWhenBedFull_expectNurseGetsBed(self):
+        sim.prioritizeNursePatient = True
+        sim.initPopulation()
+        expectedCapacity = sim.getTotalIcuBeds()
+        patients = []
+        for i in range(expectedCapacity):
+            patient = sim.Person(sim.Position(0, i))
+            patients.append(patient)
+            patient.infect(sim.Severity.INV_VENT)
+            self.assertTrue(sim.hospital.assignIcuBed(patient))
+        p = sim.Person(sim.Position(0, expectedCapacity))
+        self.assertFalse(sim.hospital.assignIcuBed(p))
+        nurse = sim.Nurse(sim.Position(0, expectedCapacity))
+        nurse.infect(sim.Severity.INV_VENT)
+        self.assertTrue(sim.hospital.assignIcuBed(nurse))
+        for nonNurse in patients:
+           self.assertTrue(nonNurse.isDead())
+           self.assertEqual(None, nonNurse.nurse)
+           self.assertTrue(nonNurse not in sim.hospital.occupiedBeds)
+        self.assertTrue(nurse in sim.hospital.occupiedBeds)
+        sim.prioritizeNursePatient = False
+        sim.initPopulation()
+
+    def test_01whenRun_expectProgressSequential(self):
+        df = sim.run()
+        for row in range(sim.height):
+           for col in range(sim.width):
+               infectedPersonDf = df[(df["x"] == col) & (df["y"] == row) & (df["wasInfected"])]
+               infectionDay = infectedPersonDf["day"].min()
+               deathDf = infectedPersonDf[infectedPersonDf["isDead"]]
+               deathDay = deathDf["day"].min()
+               if deathDay is not np.nan:
+                   isExpected = infectionDay == deathDay or infectionDay + sim.recoveryTime == deathDay
+                   if not isExpected:
+                       print(infectedPersonDf)
+                       print("deathDay - infectionDay = ", deathDay, "-", infectionDay, "=", deathDay - infectionDay)
+                       print(deathDf)
+                   self.assertTrue(isExpected)
+               else:
+                   recoveredDf = infectedPersonDf[infectedPersonDf["isRecovered"]]
+                   recoveryDay = recoveredDf["day"].min()
+                   if recoveryDay is not np.nan:
+                       isExpected = infectionDay + sim.recoveryTime == recoveryDay
+                       self.assertTrue(isExpected)
+
+    def test_01whenRun_expectNurseCountLikeProportion(self):
+        expectedNurses = round(sim.ratioNursesInPopulation * sim.populationSize) 
+        df = sim.run()
+        rowCount1 = df[(df["day"] == 1) & (df["isNurse"])].shape[0]
+        #self.assertEqual(expectedNurses, rowCount1)
+        self.assertTrue(abs(expectedNurses - rowCount1) <= 1)
+        rowCountEnd = df[(df["day"] == sim.totalDays) & (df["isNurse"])].shape[0]
+        self.assertEqual(rowCount1, rowCountEnd)
+
+    def test_02whenRun1Day_expect1Infection(self):
+        sim.totalDays = 1
+        for i in range(40):
+            df = sim.run()
+            rowCount = df[df["wasInfected"]].shape[0]
+            self.assertEqual(1, rowCount)
+
+    def test_03whenRun1Day_expect1NewlyInfected(self):
+        sim.totalDays = 1
+        df = sim.run()
+        rowCount = df[df["newlyInfected"]].shape[0]
+        self.assertEqual(1, rowCount)
+
+    def test_04whenRun2Days100InfectiousSevereCritical_expect3Dead(self):
+        sim.totalDays = 2
+        sim.infectiousness = 1
+        sim.proportionSevere = 1
+        sim.proportionSevereCritical = 1
+        df = sim.run()
+        rowCount = df[df["isDead"]].shape[0]
+        self.assertEqual(3, rowCount)
+
+    def test_05when100InfectiousNoNursesDeaths_expectNSquaredInfections(self):
+        sim.totalDays = math.floor(sim.height ** 1/2)
+        sim.infectiousness = 1
+        sim.proportionSevere = 0
+        sim.ratioNursesInPopulation = 0
+        df = sim.run()
+        rowCount = df[df["wasInfected"]].shape[0]
+        self.assertTrue(rowCount > 0)
+        rowCount = df[(df["day"] == sim.totalDays) & (df["wasInfected"])].shape[0]
+        self.assertEqual(sim.totalDays ** 2 + (sim.totalDays - 1) ** 2, rowCount)
+
+if __name__ == '__main__':
+    unittest.main()
